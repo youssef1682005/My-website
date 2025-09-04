d2f9b950b56971ca01b05c16cf4d6d5a0100ef2b
Youssef Ashraf <llaztwly@gmail.com>
2025-09-04 21:39:56 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const navLinks = document.querySelectorAll(".navbar-nav .nav-link");
+
+  navLinks.forEach((link) => {
+    link.addEventListener("click", function () {
+      navLinks.forEach((l) => l.classList.remove("active"));
+      this.classList.add("active");
+    });
+  });
+});
